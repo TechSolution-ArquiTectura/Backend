aae29ae7631b310abce8f4f20dc5c7ec14abb027
FloresIrene <u201816689@upc.edu.pe>
2023-08-30 09:02:06 -0500

    feat: upgraded availableFilm model

--- src/main/java/com/upc/TuCine/TuCine/model/AvailableFilm.java
+++ src/main/java/com/upc/TuCine/TuCine/model/AvailableFilm.java
@@ -3,19 +3,29 @@ package com.upc.TuCine.TuCine.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "AvailableFilm")
 public class AvailableFilm {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
 
     @ManyToOne
-    @JoinColumn(name = "business_id", nullable = false, foreignKey = @ForeignKey(name = "FK_TICKET_PERSON"))
+    @JoinColumn(name = "business_id", nullable = false, foreignKey = @ForeignKey(name = "FK_AVAILABLEFILM_BUSINESS"))
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Business business;
 
     @ManyToOne
-    @JoinColumn(name = "film_id", nullable = false, foreignKey = @ForeignKey(name = "FK_TICKET_SHOWTIME"))
+    @JoinColumn(name = "film_id", nullable = false, foreignKey = @ForeignKey(name = "FK_AVAILABLEFILM_FILM"))
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Film film;
 
@@ -24,9 +34,10 @@ public class AvailableFilm {
     private String customNotice;
 
     @Column(name = "is_available", nullable = false)
-    @Size(max = 1)
-    private char isAvailable;
+    private Character isAvailable;
 
-    @Column(name = "promotion_id", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "promotion_id", nullable = false, foreignKey = @ForeignKey(name = "FK_AVAILABLEFILM_PROMOTION"))
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Promotion promotion;
 }
