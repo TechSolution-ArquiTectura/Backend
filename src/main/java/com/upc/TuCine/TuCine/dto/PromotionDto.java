4703463c8f925f98199f4a2e4effad602cb27969
FloresIrene <u201816689@upc.edu.pe>
2023-08-30 09:04:01 -0500

    feat: upgraded promotionDto and showtimeDto

--- src/main/java/com/upc/TuCine/TuCine/dto/PromotionDto.java
+++ src/main/java/com/upc/TuCine/TuCine/dto/PromotionDto.java
@@ -3,18 +3,22 @@ package com.upc.TuCine.TuCine.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.upc.TuCine.TuCine.model.Business;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Data
+@Getter
+@Setter
 public class PromotionDto {
     private Integer id;
-    private String title;
+    private Business business;
+    private String name;
+    private String description;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-    private LocalDate startDate;
+    private LocalDate initDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate endDate;
-    private String description;
-    private Float discount;
-    private Business business;
+    private Float discountPercentage;
 }
