ab46bf1abbbaa257284859518196c14c6b3775d8
FloresIrene <u201816689@upc.edu.pe>
2023-08-30 09:06:17 -0500

    feat: upgraded PromotionRepository and ShowtimeRepository

--- src/main/java/com/upc/TuCine/TuCine/repository/PromotionRepository.java
+++ src/main/java/com/upc/TuCine/TuCine/repository/PromotionRepository.java
@@ -4,5 +4,5 @@ import com.upc.TuCine.TuCine.model.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
-    boolean existsPromotionByTitle(String title);
+    boolean existsPromotionByName(String name);
 }
